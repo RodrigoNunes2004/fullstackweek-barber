ed49a5cc79c255323cf2634251320a4eb157d0ce
RodrigoNunes2004 <rodrigo_samambaia@hotmail.com>
2025-04-04 22:13:05 +1300

    Feat: add Hello meessage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
- import Header from "../_components/header";
- 
- export default function Home() {
-   return (
-     <div>
-       <Header />
-     </div>
-   );
- }
+import { format } from "date-fns";
+import Header from "../_components/header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Hello, Miguel</h2>
+        <p className="capitalize text-sm">{format(new Date(), "EEEE, MMMM d, yyyy")}</p>
+      </div>
+    </div>
+  );
+}
