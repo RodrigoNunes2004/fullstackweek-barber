0ba1fb992c7592f49a4620fd4affe20deb89dd5e
RodrigoNunes2004 <rodrigo_samambaia@hotmail.com>
2025-04-22 18:23:01 +1200

    Feat add side manu, login and logout

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,27 +1,36 @@
 "use client";
 
 import Image from "next/image";
- import { Card, CardContent } from "./ui/card";
- import { Button } from "./ui/button";
- import { MenuIcon } from "lucide-react";
- import Link from "next/link";
-import { signIn, useSession } from "next-auth/react";
- 
- const Header = () => {
-  const {data} = useSession();
-  const handleLoginClick = async () => {
-    await signIn();
-  }
-   return (
-     <Card className="bg-background text-foreground">
-       <CardContent className="p-5 justify-between items-center flex flex-row">
-         <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
-          <Button variant="outline" size="icon" className="h-8 w-8">
-           <MenuIcon size={16} />
-          </Button>
-       </CardContent>
-     </Card>
-   );
- };
- 
- export default Header;
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import SideMenu from "./side-menu";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+      <Card>
+        <CardContent className="p-5 justify-between items-center flex flex-row">
+          <Link href="/">
+            <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <MenuIcon size={16} />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent className="p-0">
+              <SideMenu />
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
+  );
+};
+
+export default Header;
