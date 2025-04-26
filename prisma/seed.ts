aae3e9a6fd1eedf6d983b0c15c59dfe7b73746d8
RodrigoNunes2004 <rodrigo_samambaia@hotmail.com>
2025-04-26 13:26:07 +1200

    Chore: update .eslintrc.json file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
+// eslint-disable @typescript-eslint/no-require-imports 
+// eslint-disable 
 const { PrismaClient } = require("@prisma/client");
  
  const prisma = new PrismaClient();
