fbea9f22bec3baf8872781da9fc1cb6b5b604080
RodrigoNunes2004 <rodrigo_samambaia@hotmail.com>
2025-04-26 19:51:26 +1200

    Fix: change incompatible params  in barbershops/[id]/page.tsx

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -4,43 +4,42 @@ import ServiceItem from "./_components/service-item";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/_lib/auth";
 
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
-interface BarbershopDetailsPageProps {
-    params: {
-        id?: string;
-    };
+const BarbershopDetailsPage = async ({ params }: PageProps) => {
+  const session = await getServerSession(authOptions);
+
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
+
+  if (!barbershop) {
+    // TODO: redirect to home page
+    return null;
   }
 
-const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
-    
-    const session = await getServerSession(authOptions);
+  return (
+    <div className="px-5 flex flex-col gap-5 py-6">
+      <BarbershopInfo barbershop={barbershop} />
+      {barbershop.services.map((service) => (
+        <ServiceItem
+          key={service.id}
+          barbershop={barbershop}
+          service={service}
+          isAuthenticated={!!session?.user}
+        />
+      ))}
+    </div>
+  );
+};
 
-    if (!params.id) {
-        //TODO: redirect to home page
-        return null;
-    }
-    const barbershop = await db.barbershop.findUnique({
-        where: {
-            id: params.id
-        },
-        include: {
-            services: true
-        }
-    });
-
-    if (!barbershop) {
-        //TODO: redirect to home page
-        return null;
-    }
-    return ( 
-     <div className="px-5 flex flex-col gap-5 py-6">
-        <BarbershopInfo barbershop={barbershop} />
-
-        {barbershop.services.map((service) => (
-            <ServiceItem key={service.id} barbershop={barbershop} service={service} isAuthenticated={!!session?.user} />
-        ))}
-     </div>
-     );
-}
- 
 export default BarbershopDetailsPage;
